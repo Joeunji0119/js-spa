a1cac668ce94f6905b338a0838f02d3296ddfd8d
은지 <joeunji@eunjiui-iMac.local>
2024-05-15 12:08:54 +0900

    feat : 타입스크립트 적용중

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const app = express();
-const PORT = 3000;
-
-app.use(express.static(path.join(__dirname)));
-
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var path_1 = require("path");
+var url_1 = require("url");
+var __filename = (0, url_1.fileURLToPath)(import.meta.url);
+var __dirname = path_1.default.dirname(__filename);
+var app = (0, express_1.default)();
+var PORT = 3000;
+app.use(express_1.default.static(path_1.default.join(__dirname)));
+app.get('*', function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, 'index.html'));
 });
-
-app.listen(PORT, () => {
-	console.log(`server is running ${PORT}`);
+app.listen(PORT, function () {
+    console.log("server is running ".concat(PORT));
 });
